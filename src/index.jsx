52005e18f3c5969f3fc49d5b051d63235e87108d
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-08-10 04:07:57 +0900

    set up environment

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CalculatorContainer from './containers/CalculatorContainer';
+
+
+ReactDOM.render(
+  <CalculatorContainer />,
+  document.getElementById('root')
+);
+
