cd321d93ee45224c23e4a96f8021d1aec00a2f26
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-08-10 15:07:56 +0900

    add store

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CalculatorContainer from './containers/CalculatorContainer';
 
+import { createStore } from 'redux';
+import todoApp from './reducers/reducers';
+
+
+let store = createStore(todoApp);
 
 ReactDOM.render(
   <CalculatorContainer />,
